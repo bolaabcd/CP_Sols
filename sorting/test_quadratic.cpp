b7b20b9dfd3f11d6d911806f103cf132ad59e348
Artur <arthur.gaspar2011@gmail.com>
2022-03-11 10:21:14 -0300

    Updated quadratic sorting tests to use vector printing utility.

--- sorting/test_quadratic.cpp
+++ sorting/test_quadratic.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include "utility/print_vec.cpp"
 #include "sorting/insertion_sort.cpp"
 #include "sorting/bubble_sort.cpp"
 #include "sorting/selection_sort.cpp"
@@ -8,17 +9,25 @@ using namespace std;
 int main() {
     int vec[10] = {10,9,8,7,6,5,4,3,2,1};
     insertion_sort(vec,vec+10);
-    bubble_sort(vec,vec+10);
-    selection_sort(vec,vec+10);
-    for(int i = 0; i < 10; i++)
-        cout << vec[i] << " ";
-    cout << endl;
+    print_vec(vec,vec+10);
 
-    vector<int> vec2 = {10,9,8,7,6,5,4,3,2,1};
-    insertion_sort(vec2.begin(),vec2.end());
-    bubble_sort(vec2.begin(),vec2.end());
-    selection_sort(vec2.begin(),vec2.end());
-    for(int i = 0; i < 10; i++)
-        cout << vec2[i] << " ";
-    cout << endl;
+    int vec2[10] = {10,9,8,7,6,5,4,3,2,1};
+    bubble_sort(vec2,vec2+10);
+    print_vec(vec2,vec2+10);
+
+    int vec3[10] = {10,9,8,7,6,5,4,3,2,1};
+    selection_sort(vec3,vec3+10);
+    print_vec(vec3,vec3+10);
+
+    vector<int> vec4 = {10,9,8,7,6,5,4,3,2,1};
+    insertion_sort(vec4.begin(),vec4.end());
+    print_vec(vec4.begin(),vec4.end());
+
+    vec4 = {10,9,8,7,6,5,4,3,2,1};
+    bubble_sort(vec4.begin(),vec4.end());
+    print_vec(vec4.begin(),vec4.end());
+
+    vec4 = {10,9,8,7,6,5,4,3,2,1};
+    selection_sort(vec4.begin(),vec4.end());
+    print_vec(vec4.begin(),vec4.end());
 }
